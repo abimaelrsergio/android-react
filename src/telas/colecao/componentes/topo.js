b5bdb425688f197294047c30a3e952322ca147e6
abimaelrsergio <abimaelr.sergio@gmail.com>
2022-03-21 19:54:14 -0300

    update

---
+++ src/telas/colecao/componentes/topo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Topo() {
+    return <>
+        <Image source={topo} style={estilos.topo} />
+        <Texto style={estilos.titulo}>Detalhes da coleção de livros</Texto>
+    </>;
+}
