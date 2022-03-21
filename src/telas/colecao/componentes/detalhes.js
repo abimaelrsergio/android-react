273f8b5cda9c8ce4981c5769d7e51a488a57edd3
abimaelrsergio <abimaelr.sergio@gmail.com>
2022-03-21 20:38:08 -0300

    update

--- src/telas/colecao/componentes/detalhes.js
+++ src/telas/colecao/componentes/detalhes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 import Texto from '../../../componentes/Texto'
 import logo from '../../../../assets/logo.jpeg';
 
@@ -8,12 +8,46 @@ export default function Detalhes() {
         <Texto style={estilos.nome}>Coleção de livros</Texto>
         <View style={estilos.cabecalhoFornecedor}>
             <Image source={logo} style={estilos.imagemFornecedor} />
-            <Text style={estilos.fornecedor}>Livraria Santos Dumont</Text>
+            <Texto style={estilos.fornecedor}>Livraria Santos Dumont</Texto>
         </View>
         <Texto style={estilos.descricao}>
             Livros importandos da França, todos escritos por autores
             renomados.
         </Texto>
-        <Text style={estilos.preco}>R$ 350,00</Text>
+        <Texto style={estilos.preco}>R$ 350,00</Texto>
     </>;
 }
+const estilos = StyleSheet.create({
+    fornecedor: {
+        fontSize: 16,
+        lineHeight: 36,
+        marginLeft: 12,
+    },
+    nome: {
+        color: '#464646',
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight: 'bold'
+    },
+    imagemFornecedor: {
+        width: 31,
+        height: 31
+    },
+    cabecalhoFornecedor: {
+        flexDirection: 'row',// tudo em uma linha
+        paddingVertical: 12
+    },
+    preco: {
+        color: '#2A9F85',
+        fontWeight: 'bold',
+        fontSize: 16,
+        lineHeight: 42,
+        marginTop: 8
+    },
+    descricao: {
+        fontSize: 16,
+        lineHeight: 26,
+        color: '#A3A3A3'
+    },
+});
+
--- src/telas/colecao/index.js
+++ src/telas/colecao/index.js
@@ -13,37 +13,6 @@ export default function Colecao(){
 }
 
 const estilos = StyleSheet.create({
-    fornecedor: {
-        fontSize: 16,
-        lineHeight: 36,
-        marginLeft: 12,
-    },
-    nome: {
-        color: '#464646',
-        fontSize: 26,
-        lineHeight: 42,
-        fontWeight: 'bold'
-    },
-    imagemFornecedor: {
-        width: 31,
-        height: 31
-    },
-    cabecalhoFornecedor: {
-        flexDirection: 'row',// tudo em uma linha
-        paddingVertical: 12
-    },
-    preco: {
-        color: '#2A9F85',
-        fontWeight: 'bold',
-        fontSize: 16,
-        lineHeight: 42,
-        marginTop: 8
-    },
-    descricao: {
-        fontSize: 16,
-        lineHeight: 26,
-        color: '#A3A3A3'
-    },
     colecao: {
         paddingVertical: 10, // padding de cima e de baixo
         paddingHorizontal: 16// padding da esquerda e da direita
