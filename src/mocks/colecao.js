90879d99dd2de024f766391f4a8ac6710e5c53cf
abimaelrsergio <abimaelr.sergio@gmail.com>
2022-03-21 21:07:00 -0300

    update

---
+++ src/mocks/colecao.js
@@ -0,0 +1,13 @@
+const colecao = {
+    topo: {
+        titulo: 'Detalhes da coleção de livros'
+    },
+    detalhes: {
+        nome: 'Coleção de livros',
+        fornecedor: 'Livraria Santos Dumont',
+        descricao: 'Livros importandos da França, todos escritos por autores renomados.',
+        preco: 'R$ 350,00'
+    }
+};
+
+export default colecao;
