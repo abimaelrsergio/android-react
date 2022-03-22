819c3cdb718b71b1fc583178d13d01ea0e29965a
abimaelrsergio <abimaelr.sergio@gmail.com>
2022-03-21 21:15:49 -0300

    update

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StatusBar, StyleSheet, SafeAreaView, View } from 'react-native';
 import Colecao from './src/telas/colecao/index';
 
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import mock from './src/mocks/colecao';
 
 export default function App() {
   const [fontCarregada] = useFonts({
@@ -14,7 +15,7 @@ export default function App() {
     return <View />
   }
   return <SafeAreaView style={styles.container}>
-          <Colecao />
+          <Colecao {...mock} />
           <StatusBar />
          </SafeAreaView>;
 }
