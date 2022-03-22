91f79e851ede8c9922ee5fc97fbfce3e2740a6a9
abimaelrsergio <abimaelr.sergio@gmail.com>
2022-03-21 21:36:19 -0300

    update

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { StatusBar, StyleSheet, SafeAreaView, View } from 'react-native';
+import { StatusBar, StyleSheet, SafeAreaView } from 'react-native';
 import Colecao from './src/telas/colecao/index';
 
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 import mock from './src/mocks/colecao';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
   const [fontCarregada] = useFonts({
@@ -12,7 +13,7 @@ export default function App() {
   });
   // se a font não estiver carregada, retornauma view vazia
   if (!fontCarregada) {
-    return <View />
+    return <AppLoading />
   }
   return <SafeAreaView style={styles.container}>
           <Colecao {...mock} />
